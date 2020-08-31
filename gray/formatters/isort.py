d4177e5371021a290333cf9c8c0ef1f7fd1d6f78
IURII Shikanov <dizballanze@yandex-team.ru>
2020-08-31 11:11:36 +0300

    Fix isort imports grouping

--- gray/formatters/isort.py
+++ gray/formatters/isort.py
@@ -19,11 +19,12 @@ class SortImportsFormatter(BaseFormatter):
             "indent": "    ",
             "multi_line_output": 5,
             "length_sort": 0,
-            "default_section": "FIRSTPARTY",
+            "default_section": "THIRDPARTY",
             "virtual_env": str(arguments.isort_virtual_env),
             "include_trailing_comma": arguments.isort_include_trailing_comma,
             "lines_after_imports": arguments.isort_lines_after_imports,
             "use_parentheses": True,
+            "no_lines_before": ["LOCALFOLDER"],
         }
 
     def process(self, file_path: Path):
