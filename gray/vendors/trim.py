6f52e58f83a6c3c9a85e3d24cd7c41677dcfb60e
Artyom Nikitin <a.nikitin@edadeal.ru>
2020-11-15 16:22:13 +0300

    style: run gray

--- gray/vendors/trim.py
+++ gray/vendors/trim.py
@@ -23,20 +23,18 @@
 
 """Trims trailing whitespace from files."""
 
-from __future__ import print_function
-from __future__ import unicode_literals
 
 import io
 import os
 import sys
 
 
-__version__ = '0.4'
+__version__ = "0.4"
 
 
-CR = '\r'
-LF = '\n'
-CRLF = '\r\n'
+CR = "\r"
+LF = "\n"
+CRLF = "\r\n"
 
 MAX_READ = 1024
 MAX_LINE_LENGTH = 200
@@ -54,7 +52,8 @@ def trim(text, leading=False):
     line_ending = find_line_ending(split_lines)
 
     without_trailing = line_ending.join(
-        [line.rstrip() for line in split_lines]).rstrip() + line_ending
+        [line.rstrip() for line in split_lines],
+    ).rstrip() + line_ending
 
     if leading:
         without_trailing = without_trailing.lstrip(line_ending)
@@ -97,7 +96,7 @@ def trim_file(filename, leading=False):
     trimmed_text = trim(original_text, leading=leading)
 
     if trimmed_text != original_text:
-        with open_unicode_file(filename, 'w') as output_file:
+        with open_unicode_file(filename, "w") as output_file:
             output_file.write(trimmed_text)
 
         return True
@@ -120,34 +119,42 @@ def is_text(filename):
         with open_unicode_file(filename) as input_file:
             string = input_file.read(MAX_READ)
 
-            if not string or '\x00' in string:
+            if not string or "\x00" in string:
                 return False
 
             split_string = string.split()
             if split_string and len(split_string[0]) > MAX_LINE_LENGTH:
                 return False
-    except (IOError, UnicodeDecodeError):
+    except (OSError, UnicodeDecodeError):
         return False
 
     return True
 
 
-def open_unicode_file(filename, mode='r'):
+def open_unicode_file(filename, mode="r"):
     """Return opened Unicode file."""
-    return io.open(filename, mode=mode, encoding='utf-8',
-                   newline='')  # Preserve line endings
+    return open(
+        filename, mode=mode, encoding="utf-8",
+        newline="",
+    )  # Preserve line endings
 
 
 def main():
     """Main."""
     import argparse
-    parser = argparse.ArgumentParser(description=__doc__, prog='trim')
-    parser.add_argument('--leading-newlines', action='store_true',
-                        help='remove leading newlines too')
-    parser.add_argument('--version', action='version',
-                        version='%(prog)s ' + __version__)
-    parser.add_argument('files', nargs='+',
-                        help='files to trim')
+    parser = argparse.ArgumentParser(description=__doc__, prog="trim")
+    parser.add_argument(
+        "--leading-newlines", action="store_true",
+        help="remove leading newlines too",
+    )
+    parser.add_argument(
+        "--version", action="version",
+        version="%(prog)s " + __version__,
+    )
+    parser.add_argument(
+        "files", nargs="+",
+        help="files to trim",
+    )
 
     args = parser.parse_args()
 
@@ -156,21 +163,27 @@ def main():
         name = filenames.pop(0)
         if os.path.isdir(name):
             for root, directories, children in os.walk(unicode(name)):
-                filenames += [os.path.join(root, f) for f in children
-                              if not f.startswith('.')]
+                filenames += [
+                    os.path.join(root, f) for f in children
+                    if not f.startswith(".")
+                ]
 
-                directories[:] = [d for d in directories
-                                  if not d.startswith('.')]
+                directories[:] = [
+                    d for d in directories
+                    if not d.startswith(".")
+                ]
         else:
             try:
-                if is_text(name) and trim_file(name,
-                                               leading=args.leading_newlines):
-                    print('[file:%s]' % name, file=sys.stderr)
-            except (IOError, UnicodeDecodeError) as error:
-                print('{0}:{1}'.format(name, error), file=sys.stderr)
+                if is_text(name) and trim_file(
+                    name,
+                    leading=args.leading_newlines,
+                ):
+                    print("[file:%s]" % name, file=sys.stderr)
+            except (OSError, UnicodeDecodeError) as error:
+                print(f"{name}:{error}", file=sys.stderr)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         sys.exit(main())
     except KeyboardInterrupt:
