f27ccc958b51e9b9f67433a62abab50530955118
Stephane Odul <stephane@clumio.com>
2021-06-23 09:30:21 -0700

    Use tuple instead of list to store the formatters to use.

--- gray/formatters/composite.py
+++ gray/formatters/composite.py
@@ -6,10 +6,10 @@ from gray.formatters.base import BaseFormatter
 class CompositeFormatter(BaseFormatter):
     # Use a list to guarantee order preserving, which can be important to
     # ensure that the formatters are going to behave consistently.
-    _formatters: list
+    _formatters: tuple
 
     def __init__(self, *formatters):
-        self._formatters = list(formatters)
+        self._formatters = tuple(formatters)
 
     def process(self, file_path: Path):
         for formatter in self._formatters:
